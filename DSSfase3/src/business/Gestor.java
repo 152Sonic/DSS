48a4fac366b6025a6793c7942316701140be5a3a
158bubbles <teresinha.giao@gmail.com>
2020-12-20 19:37:03 +0000

    a

--- DSSfase3/src/business/Gestor.java
+++ DSSfase3/src/business/Gestor.java
@@ -2,42 +2,78 @@ package business;
 
 import java.util.Objects;
 
+/**
+ * Classe que contém a implementação do Gestor
+ */
 public class Gestor {
     private String nome;
     private String pass;
 
+
+    /**
+     * Construtor vazio
+     */
     public Gestor(){
         nome = new String();
         pass = new String();
     }
 
+    /**
+     * Construtor parametrizado
+     * @param nome
+     * @param pass
+     */
     public Gestor(String nome, String pass) {
         nome = nome;
         this.pass = pass;
     }
 
+    /**
+     * Construtor por cópia
+     * @param ges
+     */
     public Gestor(Gestor ges){
         nome = ges.getNome();
         pass = ges.getPass();
     }
 
+    /**
+     * Devolve o nome
+     * @return String
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Define o nome
+     * @param nome
+     */
     public void setNome(String nome) {
         nome = nome;
     }
 
+    /**
+     * Devolve a passe
+     * @return String
+     */
     public String getPass() {
         return pass;
     }
 
+    /**
+     * Define a passe
+     * @param pass
+     */
     public void setPass(String pass) {
         this.pass = pass;
     }
 
-    @Override
+    /**
+     * Verifica a igualdade com outro objeto
+     * @param o Objeto a comparar
+     * @return boolean
+     */
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -46,11 +82,18 @@ public class Gestor {
                 Objects.equals(pass, gestor.pass);
     }
 
-    @Override
+    /**
+     * Método hashCode do objeto
+     * @return hash do objeto
+     */
     public int hashCode() {
         return Objects.hash(nome, pass);
     }
 
+    /**
+     * Devolve uma cópia da instância
+     * @return
+     */
     public Gestor clone(){
         return new Gestor(this);
     }
--- DSSfase3/src/business/Leitor.java
+++ DSSfase3/src/business/Leitor.java
@@ -2,42 +2,78 @@ package business;
 
 import java.util.Objects;
 
+/**
+ * Classe que contém a implementação do Leitor
+ */
 public class  Leitor {
     private String info;
     private QRcode qrcode;
 
 
+    /**
+     * Construtor vazio
+     */
     public Leitor(){
         qrcode = new QRcode();
         info = new String();
     }
 
+    /**
+     * Construtor parametrizado
+     * @param info
+     * @param qr
+     */
     public Leitor(String info,QRcode qr) {
         this.qrcode = qr;
         this.info = info;
     }
 
+    /**
+     * Construtor por cópia
+     * @param lei
+     */
     public Leitor(Leitor lei){
         qrcode = lei.getQrcode();
         info = lei.getInfo();
     }
 
+    /**
+     * Devolve o QrCode
+     * @return QrCode
+     */
     public QRcode getQrcode() {
         return qrcode;
     }
 
+    /**
+     * Define o QrCode
+     * @param qrcode
+     */
     public void setQrcode(QRcode qrcode) {
         this.qrcode = qrcode;
     }
 
+    /**
+     * Devolve a info
+     * @return String
+     */
     public String  getInfo() {
         return info;
     }
 
+    /**
+     * Define a info
+     * @param info
+     */
     public void setInfo(String info) {
         this.info = info;
     }
 
+    /**
+     * Verifica a igualdade com outro objeto
+     * @param o Objeto a comparar
+     * @return boolean
+     */
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Leitor)) return false;
@@ -46,11 +82,18 @@ public class  Leitor {
                 Objects.equals(getQrcode(), leitor.getQrcode());
     }
 
-    @Override
+    /**
+     * Método hashCode do objeto
+     * @return hash do objeto
+     */
     public int hashCode() {
         return Objects.hash(getInfo(), getQrcode());
     }
 
+    /**
+     * Devolve uma cópia da instância
+     * @return
+     */
     public Leitor clone(){
         return new Leitor(this);
     }
